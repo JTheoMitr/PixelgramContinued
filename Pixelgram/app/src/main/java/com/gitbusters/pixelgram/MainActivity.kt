02b2784328d35f2aa7fc4ea60bacbb768fcc75fa
tylerkauff <tyler.kauffman@cognizant.com>
2022-04-11 16:09:28 -0500

    Creating theme information, post_item.xml, and app bar buttons for the landing page.

--- Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
@@ -2,10 +2,26 @@ package com.gitbusters.pixelgram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        /*Create the behavoir for clicking the */
+        // Options here
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
