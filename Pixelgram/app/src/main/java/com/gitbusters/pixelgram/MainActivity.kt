028d226f77f3fffbc493fc82c12baf73eb71fe13
John <jmitropoulos@alumni.berklee.edu>
2022-04-12 12:16:50 -0500

    Updated Gradle, Added API Interface and Retrofit Posts Call

--- Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.gitbusters.pixelgram
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import retrofit2.Retrofit
+import retrofit2.awaitResponse
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+const val BASE_URL = "http://34.134.148.105/"
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) = runBlocking {
         // Display the logo of the application
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setLogo(R.drawable.ic_pixelgram_logo)
@@ -15,8 +27,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getCurrentData()
+
 
     }
+
+    private fun getCurrentData() {
+
+        val api = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+
+        MainScope().launch(Dispatchers.IO) {
+            try {
+                val response = api.getPosts().awaitResponse()
+                if (response.isSuccessful) {
+                    val data = response.body()!!
+                    Log.d(TAG, data.content.toString())
+
+                    withContext(Dispatchers.Main) {
+                        textView.text = data.content[0].message
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(applicationContext, "no internet", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
+
+
+
+
     /* Create the buttons on the toolbar */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
@@ -31,4 +78,6 @@ class MainActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
+
+
 }
