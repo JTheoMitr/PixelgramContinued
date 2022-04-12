be744c4429f6811840eb7e4910fd35daed14ff99
tylerkauff <tyler.kauffman@cognizant.com>
2022-04-12 14:00:22 -0500

    Created post recyclerview adapter and dummy data.

--- Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // Display the logo of the application
-        supportActionBar!!.setDisplayShowHomeEnabled(true)
-        supportActionBar!!.setLogo(R.drawable.ic_pixelgram_logo)
-        supportActionBar!!.setDisplayUseLogoEnabled(true)
-        supportActionBar!!.setTitle(" Pixelgram")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setLogo()
 
+        val dummyPosts = generateDummyData()
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_post_list)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = PostRecyclerAdapter(dummyPosts)
+        recyclerView.adapter = adapter
 
     }
     /* Create the buttons on the toolbar */
@@ -31,4 +35,32 @@ class MainActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
+
+    // Sets the logo
+    fun setLogo() {
+        val toolbarTitle = " Pixelgram"
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setLogo(R.drawable.ic_pixelgram_logo)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+        supportActionBar!!.title = toolbarTitle
+    }
+
+    fun generateDummyData() : ArrayList<DummyPost> {
+        val post1 = DummyPost("Ryan", 10, 20)
+        val post2 = DummyPost("Ayman", 13, 66)
+        val post3 = DummyPost("Tyler", 1, 123)
+
+        val list = arrayListOf<DummyPost>()
+        list.add(post1)
+        list.add(post2)
+        list.add(post3)
+        return list
+    }
 }
+
+// For generating some dummy data.
+class DummyPost(
+    val username: String,
+    val commentCount: Int,
+    val likeCount: Int
+)
---
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/PostRecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package com.gitbusters.pixelgram
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+// Build the recyclerview with post_items
+class PostRecyclerAdapter (private val postData: ArrayList<DummyPost>) : RecyclerView.Adapter<PostRecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val username : TextView = view.findViewById(R.id.username)
+        val likeCount : TextView = view.findViewById(R.id.tv_like_count)
+        val commentCount : TextView = view.findViewById(R.id.tv_comment_count)
+        val commentList : LinearLayout = view.findViewById(R.id.ll_comment_list)
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            // In this case, clicking the post(?)
+
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.post_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents within the view, in this case the post information.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Set the contents of the view.
+        // Get the current post
+        val post = postData[position]
+        holder.username.text = post.username
+        holder.likeCount.text = post.likeCount.toString() + " Likes"
+        holder.commentCount.text = post.commentCount.toString() + " Comments"
+    }
+
+    override fun getItemCount() = postData.size
+}
+
+// Class for generating dummy data to test formatting
