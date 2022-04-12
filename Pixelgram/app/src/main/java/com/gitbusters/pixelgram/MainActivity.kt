4eb73bde93de06c4078b8730a47bc0f173e1f0a1
tylerkauff <tyler.kauffman@cognizant.com>
2022-04-12 09:36:42 -0500

    Added toolbar logo to landing page.

--- Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/MainActivity.kt
@@ -7,19 +7,26 @@ import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Display the logo of the application
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setLogo(R.drawable.ic_pixelgram_logo)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+        supportActionBar!!.setTitle(" Pixelgram")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+
+    }
+    /* Create the buttons on the toolbar */
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
 
+    /* Create the behavoir for clicking the toolbar buttons */
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
-        /*Create the behavoir for clicking the */
-        // Options here
+        // TODO create click behavoirs
         else -> {
             super.onOptionsItemSelected(item)
         }
