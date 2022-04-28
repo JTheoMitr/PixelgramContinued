39cc917ef7aaaabefb31e607048c9587a7dca769
ccastorena-cogn <99215403+ccastorena-cogn@users.noreply.github.com>
2022-04-28 13:48:58 -0500

    Red Error Outlines for boxes that are not completed. If the box is completed, it will remove the red outline but keep it if another box stays empty. If ALL boxes are complete, it will then check to see if the passwords match. If they do, you are directed to the MainActivity. Otherwise, it will give a toast message saying they don't match. Divider Line added b/w logo and "Register"

--- Pixelgram/app/src/main/java/com/gitbusters/pixelgram/UserRegister.kt
+++ Pixelgram/app/src/main/java/com/gitbusters/pixelgram/UserRegister.kt
@@ -17,10 +17,9 @@ class UserRegister : AppCompatActivity() {
         setContentView(R.layout.activity_user_register)
 
         val submit: Button = findViewById(R.id.submit_button)
-        val passwordVar: EditText = findViewById(R.id.password)
-        val password2Var: EditText = findViewById(R.id.password2)
+        val passwordVar: EditText = findViewById(R.id.editTextPassword)
+        val password2Var: EditText = findViewById(R.id.editTextPassword2)
         val usernameVar : EditText = findViewById(R.id.editTextUsername)
-
 
 
         submit.setOnClickListener {
@@ -28,41 +27,66 @@ class UserRegister : AppCompatActivity() {
             val password2 = password2Var.text.toString()
             val username = usernameVar.text.toString()
 
-            if (username == "" || password == "" || password2 == "") {
-                Toast.makeText(applicationContext, "Please Complete All Fields", Toast.LENGTH_LONG).show()
-            } else {
-                if (password == password2) {
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
+            fun checkPasswords() {
+                if (username != "" && password != "" && password2 != "") {
+                    if (password == password2) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.d("INFO", "Test")
+                        Toast.makeText(applicationContext, "Passwords do not match", Toast.LENGTH_LONG).show()
+                    }
                 } else {
-                    Log.d("INFO", "Test")
-                    Toast.makeText(applicationContext, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Please Complete All Fields", Toast.LENGTH_LONG).show()
                 }
+            }
+
+            fun checkIfEmpty(s: String, editText: EditText) {
+                if (s == "") {
+                    editText.setError("Please Complete All Fields")
+                    editText.setBackgroundResource(R.drawable.red_outline)
+                    Toast.makeText(applicationContext, "Please Complete All Fields", Toast.LENGTH_LONG).show()
+                } else if (s != "") {
+                    editText.setBackgroundResource(R.drawable.filled_border)
+                    checkPasswords()
+                }
+            }
+            checkIfEmpty(username, editTextUsername)
+            checkIfEmpty(password, editTextPassword)
+            checkIfEmpty(password2, editTextPassword2)
+        }
+
+        fun checkIfEmpty(s: String, editText: EditText) {
+            if (s == "") {
+                editText.setError("Please Complete All Fields")
+                editText.setBackgroundResource(R.drawable.red_outline)
             }
         }
 
+
         showHideBtn.setOnClickListener {
+
             if (showHideBtn.text.toString().equals("SHOW")) {
-                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                password2.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                editTextPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                editTextPassword2.transformationMethod = HideReturnsTransformationMethod.getInstance()
                 showHideBtn.text = getString(R.string.hide)
                 showHideBtn2.text = getString(R.string.hide)
             } else {
-                password.transformationMethod = PasswordTransformationMethod.getInstance()
-                password2.transformationMethod = PasswordTransformationMethod.getInstance()
+                editTextPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                editTextPassword2.transformationMethod = PasswordTransformationMethod.getInstance()
                 showHideBtn.text = getString(R.string.show)
                 showHideBtn2.text = getString(R.string.show)
             }
         }
         showHideBtn2.setOnClickListener {
             if (showHideBtn.text.toString().equals("SHOW")) {
-                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                password2.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                editTextPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                editTextPassword2.transformationMethod = HideReturnsTransformationMethod.getInstance()
                 showHideBtn.text = getString(R.string.hide)
                 showHideBtn2.text = getString(R.string.hide)
             } else {
-                password.transformationMethod = PasswordTransformationMethod.getInstance()
-                password2.transformationMethod = PasswordTransformationMethod.getInstance()
+                editTextPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                editTextPassword2.transformationMethod = PasswordTransformationMethod.getInstance()
                 showHideBtn.text = getString(R.string.show)
                 showHideBtn2.text = getString(R.string.show)
             }
